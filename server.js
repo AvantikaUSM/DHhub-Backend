9be339aaae3c8fd5f9e67432418f7466855bb9fa
AvantikaUSM <avantika.mittapally@usm.edu>
2025-02-07 16:38:17 -0600

    changes cors-1

--- server.js
+++ server.js
@@ -27,13 +27,9 @@ const allowedOrigins=[
 ]
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: allowedOrigins,
+
+   
     credentials: true,
     methods: "GET,POST,PUT,DELETE,PATCH,OPTIONS",
     allowedHeaders: "Content-Type,Authorization"
